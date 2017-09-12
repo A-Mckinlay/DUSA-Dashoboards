fd446942655391176787f536b644fce22ef4cd18
Arkan Emberwalker <arkan@drakon.io>
2017-09-12 10:51:22 +0100

    Start of test infrastructure, fix stupid test compile vs runtime scopes.

---
+++ src/test/java/uk/ac/dundee/ac41004/team9/test/ExampleTest.java
@@ -0,0 +1,13 @@
+package uk.ac.dundee.ac41004.team9.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void exampleTest() {
+        Assertions.assertTrue(true);
+    }
+
+}
