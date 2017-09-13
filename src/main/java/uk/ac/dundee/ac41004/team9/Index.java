d13ad1474f3d8e1ddea9c276663a920cb634c0c5
A-Mckinlay <a.mckinlay@dundee.ac.uk>
2017-09-13 15:47:05 +0100

    Added templates directory and example route for index page

---
+++ src/main/java/uk/ac/dundee/ac41004/team9/Index.java
@@ -0,0 +1,18 @@
+package uk.ac.dundee.ac41004.team9;
+
+import io.drakon.spark.autorouter.Routes;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Index {
+    @Routes.GET(path="/")
+    public static Object indexRoute(Request req, Response res){
+        Map<String, Object> model = new HashMap<>();
+        return new MustacheTemplateEngine().render(new ModelAndView(model, "/templates/index.mustache"));
+    }
+}
