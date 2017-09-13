0d567f17635b04c5e02ad7ef21dc51ffbae435ba
Arkan Emberwalker <arkan@drakon.io>
2017-09-13 16:08:00 +0100

    Now with Render helpers.

--- src/main/java/uk/ac/dundee/ac41004/team9/Index.java
+++ src/main/java/uk/ac/dundee/ac41004/team9/Index.java
@@ -1,18 +1,14 @@
 package uk.ac.dundee.ac41004.team9;
 
+import static uk.ac.dundee.ac41004.team9.Render.*;
+
 import io.drakon.spark.autorouter.Routes;
-import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
-import spark.template.mustache.MustacheTemplateEngine;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class Index {
     @Routes.GET(path="/")
-    public static Object indexRoute(Request req, Response res){
-        Map<String, Object> model = new HashMap<>();
-        return new MustacheTemplateEngine().render(new ModelAndView(model, "index.mustache"));
+    public static Object indexRoute(Request req, Response res) {
+        return mustache("index");
     }
 }
---
+++ src/main/java/uk/ac/dundee/ac41004/team9/Render.java
@@ -0,0 +1,21 @@
+package uk.ac.dundee.ac41004.team9;
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Render {
+
+    private Render() {} // Static utils
+
+    public static String mustache(String template, Map<String, Object> model) {
+        return new MustacheTemplateEngine().render(new ModelAndView(model, template + ".mustache"));
+    }
+
+    public static String mustache(String template) {
+        return mustache(template, new HashMap<>());
+    }
+
+}
