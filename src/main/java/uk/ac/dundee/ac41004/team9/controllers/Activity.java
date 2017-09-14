42bd0e3d836a074a17b71b9756d2dfd9f3f08f88
Erin Coey <ecoey332@gmail.com>
2017-09-14 11:04:12 +0100

    Added mustache files for each page we created during planning stages and added controller with router

--- src/main/java/uk/ac/dundee/ac41004/team9/controllers/Activity.java
+++ src/main/java/uk/ac/dundee/ac41004/team9/controllers/Activity.java
@@ -6,9 +6,9 @@ import spark.Response;
 
 import static uk.ac.dundee.ac41004.team9.Render.mustache;
 
-public class Login {
-    @Routes.GET(path="/login")
-    public static Object loginRoute(Request req, Response res) {
-        return mustache("login");
+public class Activity {
+    @Routes.GET(path="/activity")
+    public static Object activityRoute(Request req, Response res) {
+        return mustache("activity");
     }
 }
