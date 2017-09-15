66a8771d74e1e4118fe5d56c97cd67ec53ea168f
Arkan Emberwalker <arkan@drakon.io>
2017-09-15 12:36:32 +0100

    Login error alerts.

--- src/main/java/uk/ac/dundee/ac41004/team9/SecurityManager.java
+++ src/main/java/uk/ac/dundee/ac41004/team9/SecurityManager.java
@@ -6,6 +6,9 @@ import io.drakon.spark.syn.Syn;
 import lombok.experimental.UtilityClass;
 import spark.Redirect;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @UtilityClass
 public class SecurityManager {
 
@@ -19,7 +22,27 @@ public class SecurityManager {
     public static Syn getSyn() {
         if (SYN_INST == null) {
             SYN_INST = new Syn("/login", new MemeticAuthProvider(), false,
-                    (req, res) -> Render.mustache("login"),
+                    (req, res) -> {
+                        Map<String, Object> model = new HashMap<>();
+                        if (req.attribute("syn-error") != null) {
+                            String s;
+                            switch ((Syn.ErrorState)req.attribute("syn-error")) {
+                                case NO_SUCH_USER:
+                                    s = "No such user.";
+                                    break;
+                                case MISSING_FIELD:
+                                    s = "A required field was empty.";
+                                    break;
+                                case INVALID_CREDENTIALS:
+                                    s = "Check your password and try again.";
+                                    break;
+                                default:
+                                    s = "An unknown error occured.";
+                            }
+                            model.put("error", s);
+                        }
+                        return Render.mustache("login", model);
+                    },
                     (req, res) -> { res.redirect("/", Redirect.Status.FOUND.intValue()); return null; });
             SYN_INST.createUser("test", "test");
         }
