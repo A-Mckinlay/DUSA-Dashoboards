b55c586134d5065012a55b3c7e09992270102a86
Arkan Emberwalker <arkan@drakon.io>
2017-09-18 13:21:09 +0100

    Logout route.

---
+++ src/main/java/uk/ac/dundee/ac41004/team9/controllers/Logout.java
@@ -0,0 +1,22 @@
+package uk.ac.dundee.ac41004.team9.controllers;
+
+import io.drakon.spark.autorouter.Routes;
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+import spark.Request;
+import spark.Response;
+import uk.ac.dundee.ac41004.team9.SecurityManager;
+
+import static uk.ac.dundee.ac41004.team9.Render.mustache;
+
+@UtilityClass
+@Slf4j
+public class Logout {
+
+    @Routes.GET(path = "/logout")
+    public static Object logout(Request req, Response res) {
+        SecurityManager.getSyn().logoutUser(req.attribute("user"), req, res);
+        return mustache("logout");
+    }
+
+}
