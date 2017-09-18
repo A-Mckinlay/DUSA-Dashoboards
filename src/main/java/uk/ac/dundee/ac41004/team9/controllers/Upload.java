a2b5e2943ba93d974a4fd165b178b7cf38f1d802
Arkan Emberwalker <arkan@drakon.io>
2017-09-18 15:34:59 +0100

    Proper error/status rendering in upload.

--- src/main/java/uk/ac/dundee/ac41004/team9/controllers/Upload.java
+++ src/main/java/uk/ac/dundee/ac41004/team9/controllers/Upload.java
@@ -4,7 +4,6 @@ import io.drakon.spark.autorouter.Routes;
 import lombok.Lombok;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
-import spark.HaltException;
 import spark.Request;
 import spark.Response;
 import uk.ac.dundee.ac41004.team9.Config;
@@ -22,6 +21,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
 
 import static spark.Spark.halt;
@@ -55,30 +55,53 @@ public class Upload {
         try (InputStream is = req.raw().getPart("disbursal-file").getInputStream()) {
             List<YoyoWeekSpreadsheetRow> disbursalSheet = YoyoXSSFParser.parseSheet(is);
             if (alreadyExists(disbursalSheet)) {
-                throw halt(400, "sample already uploaded");
+                return renderStatus(req, res, 400, "File Already Uploaded",
+                        "This files data is already in the system.", "warning");
             }
             boolean success = DBIngest.uploadRowsToDB(disbursalSheet);
-            if (!success) throw halt(500, "something went WRONG D:");
-            return "upload complete"; // TODO: Proper success page
-        } catch (HaltException ex) {
-            throw ex; // Propagate Spark halts
+            if (!success) {
+                return renderStatus(req, res, 500,
+                        "Server Error", "The server was unable to upload your data to its database. Ask your system " +
+                                "administrator for assistance if this persists.", "danger");
+            }
+            return renderStatus(req, res, 200, "Success",
+                    "Your file has been uploaded. The new data should appear in your dashboards shortly.", "success");
         } catch (IOException | ServletException e) {
             log.error("upload failed", e);
-            res.status(500);
-            return res;
+            return renderStatus(req, res, 500,
+                    "Upload Failed", "The server was unable to receive your file.", "danger");
         } catch (YoyoParseException e) {
             log.error("upload failed; parse error.", e);
             res.status(400);
-            return "invalid or too large file"; // TODO: Proper error page
+            return renderStatus(req, res, 400,
+                    "Invalid File", "The file you uploaded could not be parsed. Check the file you selected is a " +
+                            "valid Yoyo transactions Excel file.", "warning");
         } catch (SQLException ex) {
             log.error("SQL exception in upload.", ex);
-            res.status(500);
-            return "database access error";
+            return renderStatus(req, res, 500,
+                    "Server Error", "The server was unable to upload your data to its database. Ask your system " +
+                            "administrator for assistance if this persists.", "danger");
+        } catch (IllegalStateException ex) {
+            // File too large
+            log.error("File too large!");
+            return renderStatus(req, res, 400,
+                    "File Too Large", "The file you selected is too large to upload through this page.", "warning");
         } catch (Exception ex) {
-            log.error("error in upload; probably invalid file.", ex);
-            res.status(400);
-            return "invalid file"; // TODO: Proper error page
+            log.error("error in upload; unrecognised exception type.", ex);
+            return renderStatus(req, res, 500,
+                    "Server Error", "The server encountered a problem handling your upload. Contact your system " +
+                            "administrator for assistance.", "danger");
         }
+    }
+
+    private static Object renderStatus(Request req, Response res, int status, String renderTitle, String renderText,
+                                       String renderClass) {
+        res.status(status);
+        HashMap<String, Object> model = new HashMap<>();
+        model.put("renderTitle", renderTitle);
+        model.put("renderText", renderText);
+        model.put("renderClass", renderClass);
+        return mustache(req, "upload", model);
     }
 
     @SuppressWarnings("ConstantConditions")
