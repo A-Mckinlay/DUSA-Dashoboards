3b6080f96ab0309481029c2006de41d67bcfbcd9
Allan McKinlay <a.mckinlay@dundee.ac.uk>
2017-09-25 00:39:16 +0100

    Hooked up dashboard top 5 venues bargraph to DB

--- src/main/resources/static/js/activity_summary_venue_pop.js
+++ src/main/resources/static/js/activity_summary_venue_pop.js
@@ -1,104 +1,103 @@
-requirejs(["moment", "Chart", "lodash", "dashohelper", "chroma", "distinct-colors"],
-    function (moment, Chart, _, Helper, chroma, distinctColors) {
+requirejs(["moment", "Chart", "lodash", "dashohelper", "chroma", "distinct-colors"], function (moment, Chart, _, Helper, chroma, distinctColors) {
 
     function rebuild() {
-            let promise = Helper.get("/api/meta/latestdate");
-            promise.then(function (latestDate) {
-                let dateRange = Helper.createDateRangeObj(latestDate, 7);
-                const getParams = "start=" + dateRange.start.toISOString() + "&end=" + dateRange.end.toISOString();
-                return Helper.get(encodeURI("/api/sales/txsummary?" + getParams));
-            }).then(function(graphData) {
-                drawVenuePopGraph(graphData);
-            }).catch(function (error) {
-                console.log(error);
-            });
-        }
+        let promise = Helper.get("/api/meta/latestdate");
+        promise.then(function (latestDate) {
+            let dateRange = Helper.createDateRangeObj(latestDate, 7);
+            const getParams = "start=" + dateRange.start.toISOString() + "&end=" + dateRange.end.toISOString();
+            return Helper.get(encodeURI("/api/sales/txsummary?" + getParams));
+        }).then(function(graphData) {
+            drawVenuePopGraph(graphData);
+        }).catch(function (error) {
+            console.log(error);
+        });
+    }
 
-        function drawVenuePopGraph(graphData){
-            const rawData = JSON.parse(graphData);
-            let hlabels = parseTotalSalesDataSetLabels(rawData);
-            let colourPallete = getColorPallete(hlabels.length);
-            let dataSet = parseTotalSalesDataSet(rawData);
-            drawChart("venue-popularity-canvas", "Revenue by Venue Over the Last 7 Days", hlabels, colourPallete, dataSet);
-        }
+    function drawVenuePopGraph(graphData){
+        const rawData = JSON.parse(graphData);
+        let hlabels = parseTotalSalesDataSetLabels(rawData);
+        let colourPallete = getColorPallete(hlabels.length);
+        let dataSet = parseTotalSalesDataSet(rawData);
+        drawChart("venue-popularity-canvas", "Revenue by Venue Over the Last 7 Days", hlabels, colourPallete, dataSet);
+    }
 
-        function parseTotalSalesDataSetLabels(raw) {
-            let outlets = [];
-            _.each(raw, function (v) {
-                    outlets.push(v.outletname);
-            });
-            return outlets;
-        }
+    function parseTotalSalesDataSetLabels(raw) {
+        let outlets = [];
+        _.each(raw, function (v) {
+                outlets.push(v.outletname);
+        });
+        return outlets;
+    }
 
-        function parseTotalSalesDataSet(raw) {
-            _.each(raw, function (v) {
-                delete v.cashspent;
-                delete v.discountamount;
-                v.x = v.outletname;
-                v.y = v.totalamount;
-                delete  v.outletname;
-                delete  v.totalamount;
-            });
-            return raw;
-        }
+    function parseTotalSalesDataSet(raw) {//Note: creating the property x and y here so that the object can be interpreted by Charts.js lib.
+        _.each(raw, function (v) {
+            delete v.cashspent;
+            delete v.discountamount;
+            v.x = v.outletname;
+            v.y = v.totalamount;
+            delete  v.outletname;
+            delete  v.totalamount;
+        });
+        return raw;
+    }
 
-        function getColorPallete(numOfColours){
-            let chartPallete = distinctColors({count: numOfColours, lightMin: 50, chromaMin: 50});
-            let flattenedPallete = [];
-            _.each(chartPallete, function(value){
-                let rgbCode = value._rgb.toString();
-                let palleteEntry = "rgba("+rgbCode+")";
-                flattenedPallete.push(palleteEntry);
-            });
-            return flattenedPallete;
-        }
+    function getColorPallete(numOfColours){
+        let chartPallete = distinctColors({count: numOfColours, lightMin: 50, chromaMin: 50});
+        let flattenedPallete = [];
+        _.each(chartPallete, function(value){
+            let rgbCode = value._rgb.toString();
+            let palleteEntry = "rgba("+rgbCode+")";
+            flattenedPallete.push(palleteEntry);
+        });
+        return flattenedPallete;
+    }
 
-        function drawChart(selector, title, hLabels, colourPallete, dataset) {
-            let chartConfig = {
-                type: 'bar',
-                data: {
-                    labels: hLabels,
-                    datasets: [{
-                        label: "BarchartData",
-                        data: dataset,
-                        backgroundColor: colourPallete,
-                        borderColor: colourPallete,
-                        borderWidth: 1
+    function drawChart(selector, title, hLabels, colourPallete, dataset) {
+        let chartConfig = {
+            type: 'bar',
+            data: {
+                labels: hLabels,
+                datasets: [{
+                    label: "BarchartData",
+                    data: dataset,
+                    backgroundColor: colourPallete,
+                    borderColor: colourPallete,
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: true,
+                title:{
+                    display: true,
+                    text: title
+                },
+                legend: {
+                    display: false
+                },
+                scales: {
+                    xAxes: [{ticks: {
+                        autoSkip: false
+                    }}],
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        },
+                        scaleLabel:{
+                            display: true,
+                            labelString: "£"
+                        }
                     }]
                 },
-                options: {
-                    responsive: true,
-                    maintainAspectRatio: true,
-                    title:{
-                        display: true,
-                        text: title
-                    },
-                    legend: {
-                        display: false
-                    },
-                    scales: {
-                        xAxes: [{ticks: {
-                            autoSkip: false
-                        }}],
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            },
-                            scaleLabel:{
-                                display: true,
-                                labelString: "£"
-                            }
-                        }]
-                    },
-                }
-            };
+            }
+        };
 
-            console.debug("CHART CFG:");
-            console.debug(chartConfig);
+        console.debug("CHART CFG:");
+        console.debug(chartConfig);
 
-            let ctx = document.getElementById(selector);
-            new Chart(ctx, chartConfig);
-        }
+        let ctx = document.getElementById(selector);
+        new Chart(ctx, chartConfig);
+    }
 
 window.onload += rebuild();
 // END requirejs
--- src/main/resources/static/js/top5BarGraph.js
+++ src/main/resources/static/js/top5BarGraph.js
@@ -1,45 +1,105 @@
-requirejs(["moment", "Chart"], function (moment, Chart) {
-    var ctx = document.getElementById("top5BarGraph").getContext('2d');
-    var top5BarGraph = new Chart(ctx, {
-        type: 'horizontalBar',
-        data: {
-            labels: ["Mono", "Library", "Air Bar", "Entertainment", "Food on Four"],
-            datasets: [{
-                label: 'Total Sales - £',
-                data: [12, 19, 3, 5, 2],
-                backgroundColor: [
-                    'rgb(255, 99, 132)',
-                    'rgb(54, 162, 235)',
-                    'rgb(255, 206, 86)',
-                    'rgb(75, 192, 192)',
-                    'rgb(153, 102, 255)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                xAxes: [{scaleLabel:{
-                    display: true,
-                    labelString: "Total Sales - £"
-                }}],
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
+requirejs(["moment", "Chart", "lodash", "dashohelper", "chroma", "distinct-colors"], function (moment, Chart, _, Helper, chroma, distinctColors) {
+
+    function rebuild() {
+        let promise = Helper.get("/api/meta/latestdate");
+        promise.then(function (latestDate) {
+            let dateRange = Helper.createDateRangeObj(latestDate, 7);
+            const getParams = "start=" + dateRange.start.toISOString() + "&end=" + dateRange.end.toISOString();
+            return Helper.get(encodeURI("/api/sales/txsummary?" + getParams));
+        }).then(function(graphData) {
+            drawVenuePopGraph(graphData);
+        }).catch(function (error) {
+            console.log(error);
+        });
+    }
+
+    function drawVenuePopGraph(graphData){
+        const rawData = JSON.parse(graphData);
+        let topFiveData = parseTotalSalesDataSet(rawData);
+        let vlabels = parseTotalSalesDataSetLabels(topFiveData);
+        let colourPallete = getColorPallete(vlabels.length);
+        drawChart("top5BarGraph", "Revenue by Venue Over the Last 7 Days", vlabels, colourPallete, topFiveData);
+    }
+
+    function parseTotalSalesDataSetLabels(topFiveData) {
+        let outlets = [];
+        _.each(topFiveData, function (v) {
+            outlets.push(v.y);
+        });
+        return outlets;
+    }
+
+    function parseTotalSalesDataSet(raw) {
+        _.each(raw, function (v) { //Note: creating the property x and y here so that the object can be interpreted by Charts.js lib.
+            delete v.cashspent;
+            delete v.discountamount;
+            if(v.outletname == "DUSA The Union - Marketplace") {
+                v.y = "Marketplace";
+            }
+            else{
+                v.y = v.outletname;
+            }
+            v.x = v.totalamount;
+            delete  v.outletname;
+            delete  v.totalamount;
+        });
+        raw = _.sortBy(raw, function (entry) {//sort to ascending
+            return entry.x;
+        });
+        raw.reverse();//change sort to descending
+        raw.length = 5;
+        return raw;
+    }
+
+    function getColorPallete(numOfColours){
+        let chartPallete = distinctColors({count: numOfColours, lightMin: 50, chromaMin: 50});
+        let flattenedPallete = [];
+        _.each(chartPallete, function(value){
+            let rgbCode = value._rgb.toString();
+            let palleteEntry = "rgba("+rgbCode+")";
+            flattenedPallete.push(palleteEntry);
+        });
+        return flattenedPallete;
+    }
+
+    function drawChart(selector, title, vlabels, colourPallete, dataset){
+        let chartConfig = {
+            type: 'horizontalBar',
+            data: {
+                labels: vlabels,
+                datasets: [{
+                    label: 'Total Sales - £',
+                    data: dataset,
+                    backgroundColor: colourPallete,
+                    borderColor: colourPallete,
+                    borderWidth: 1
                 }]
             },
-            legend:{
-                display: false
+            options: {
+                responsive: true,
+                maintainAspectRatio: true,
+                scales: {
+                    xAxes: [{scaleLabel:{
+                        display: true,
+                        labelString: "Total Sales - £"
+                    }}],
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                },
+                legend:{
+                    display: false
+                }
             }
-        }
-    });
+        };
+        console.debug("CHART CFG:");
+        console.debug(chartConfig);
+        let ctx = document.getElementById(selector);
+        new Chart(ctx, chartConfig);
+    }
 
+    window.onload += rebuild();
+// END requirejs
 });
